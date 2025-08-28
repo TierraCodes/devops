2915c305a2b140acd1a343b914bc8f2ad4857fcb
Tierra Williams <112350897+TierraCodes@users.noreply.github.com>
2025-08-28 14:06:41 +0000

    added a new test

--- test/test.js
+++ test/test.js
@@ -12,3 +12,10 @@ describe('First two terms', function() {
       assert.strictEqual(main.fibonacci(1), 1);
     });
 });
+
+describe('Second two terms', function() {
+  it('should return 1 and 2 for second two terms', function() {
+      assert.strictEqual(main.fibonacci(2), 1);
+      assert.strictEqual(main.fibonacci(3), 2);
+    });
+});
